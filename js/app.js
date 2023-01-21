cd3ec0cffb0c42f811bc85c8d6e554403e73e7fd
kofuku yusuke <yusuke07@DESKTOP-B1D7MH6.localdomain>
2023-01-21 16:39:22 +0900

    first commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+/**
+$(function(){
+    var $script = $('<script>').attr({
+      'type': 'text/javascript',
+      'src': './xxx.json.js'
+    });
+    $script.on('load', function(){
+      var data = window.SWEETS;
+      /// 読み込みデータをゴニョゴニョ...
+    });
+    $('body')[0].appendChild(script[0]);
+  });
+*/
---
+++ js/name.js
@@ -0,0 +1,39 @@
+// ポケモンの日本語、英語名、jsonファイル
+[
+    {
+        "ja": "フシギダネ",
+        "en": "Bulbasaur"
+    },
+    {
+        "ja": "フシギソウ",
+        "en": "Ivysaur"
+    },
+    {
+        "ja": "フシギバナ",
+        "en": "Venusaur"
+    },
+    {
+        "ja": "ヒトカゲ",
+        "en": "Charmander"
+    },
+    {
+        "ja": "リザード",
+        "en": "Charmeleon"
+    },
+    {
+        "ja": "リザードン",
+        "en": "Charizard"
+    },
+    {
+        "ja": "ゼニガメ",
+        "en": "Squirtle"
+    },
+    {
+        "ja": "カメール",
+        "en": "Wartortle"
+    },
+    {
+        "ja": "カメックス",
+        "en": "Blastoise"
+    }
+]
